294068d6a3e4651627c5267e6ae9fb9058cc8af4
Rdnator555 <ricardo.rgm873@gmail.com>
2023-07-18 19:39:40 +0200

    Revert "v2"
    This reverts commit 5f6789878a2188b783ec8c22a8260180918f698f.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Lovesick
+TLDR: I dont get paid enough to write the lore so "Chicken Thoughs"
--- lovesick_source/player_scripts.lua
+++
@@ -1,8 +0,0 @@
-local PlayerCode = {
-    
-    Faithfull = require("lovesick_source.characters.Faithfull"),
-    --Fatefull = require("")
-    Snowball = require("lovesick_source.characters.Snowball"),
-}
-
-return PlayerCode
